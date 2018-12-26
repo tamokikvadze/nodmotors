26e246d92000912cece36ab5f67bdacf0e73f855
tamokikvadze <46157364+tamokikvadze@users.noreply.github.com>
2018-12-26 14:57:26 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper (".swiper-container", {
+	effect: "fade",
+	loop: true,
+	fadeEffect: {
+		crossFade: true
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	}
+
+});
